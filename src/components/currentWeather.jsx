565dfc183f9759426cac9c9288ec3d604f412d3e
Markie Choi <markie054@gmail.com>
2022-04-07 12:31:43 +0900

    Added background and changed icon opacity

--- src/components/currentWeather.jsx
+++ src/components/currentWeather.jsx
@@ -20,17 +20,19 @@ export const CurrentWeather = ({ locationKey }) => {
 
   return (
     weatherData && (
-      <div className=''>
+      <div className='mt-6 mx-10 h-full bg-center bg-cover rounded' style={{
+        backgroundImage: `url(./skybg.jpg)`
+      }}>
         <h1 className='text-center font-bold text-xl p-6'>Current Weather</h1>
-        <div className='flex justify-center gap-x-6 h-20'>
-          <div className='flex justify-center'>
-            <div>
-              <img className='h-full mb-3' src={`${process.env.REACT_APP_PUBLIC_URL}/weatherIcons/${weatherData.WeatherIcon}.png`} alt="icon" />
-              <p className='text-center'>{weatherData.WeatherText}</p>
+        <div className='flex justify-center gap-x-6'>
+          <div className='flex w-1/3 justify-around items-center'>
+            <div className='flex flex-col h-full'>
+              <img className='pb-3 opacity-80' src={`${process.env.REACT_APP_PUBLIC_URL}/weatherIcons/${weatherData.WeatherIcon}.png`} alt="icon" />
+              <p className='text-center mb-3'>{weatherData.WeatherText}</p>
             </div>
-            <p className='text-center text-red-400 text-5xl p-6'>{weatherData.Temperature.Metric.Value} °{weatherData.Temperature.Metric.Unit}</p>
+            <p className='text-center text-red-400 text-5xl'>{weatherData.Temperature.Metric.Value} °{weatherData.Temperature.Metric.Unit}</p>
           </div>
-          <div className='gap-x-4 mt-3'>
+          <div className=''>
             <p className=''>Wind: {weatherData.Wind.Direction.Degrees}/ {weatherData.Wind.Direction.English}</p>
             <p>Humidty: {weatherData.RelativeHumidity}</p>
             <p>Visibility: {weatherData.Visibility.Metric.Value} {weatherData.Visibility.Metric.Unit}</p>
