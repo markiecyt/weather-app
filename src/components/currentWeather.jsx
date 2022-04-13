ddb433bf6c989da8e0c7df6f09acf6753620075d
Markie Choi <markie054@gmail.com>
2022-04-13 10:50:34 +0900

    Text color changed and adjusted the card

--- src/components/currentWeather.jsx
+++ src/components/currentWeather.jsx
@@ -20,19 +20,19 @@ export const CurrentWeather = ({ locationKey }) => {
 
   return (
     weatherData && (
-      <div className='mt-6 mx-10 h-full bg-center bg-cover rounded' style={{
+      <div className='mt-6 mx-20 h-full bg-center bg-cover rounded-md' style={{
         backgroundImage: `url(./skybg.jpg)`
       }}>
-        <h1 className='text-center font-bold text-xl p-6'>Current Weather</h1>
+        <h1 className='text-center font-bold font-serif text-indigo-50 text-2xl p-6'>Current Weather</h1>
         <div className='flex justify-center gap-x-6'>
           <div className='flex w-1/3 justify-around items-center'>
             <div className='flex flex-col h-full'>
               <img className='pb-3 opacity-80' src={`${process.env.REACT_APP_PUBLIC_URL}/weatherIcons/${weatherData.WeatherIcon}.png`} alt="icon" />
-              <p className='text-center mb-3'>{weatherData.WeatherText}</p>
+              <p className='text-center font-sans text-sm text-indigo-50 mb-3'>{weatherData.WeatherText}</p>
             </div>
-            <p className='text-center text-red-400 text-5xl'>{weatherData.Temperature.Metric.Value} °{weatherData.Temperature.Metric.Unit}</p>
+            <p className='text-center text-indigo-50 text-5xl'>{weatherData.Temperature.Metric.Value} °{weatherData.Temperature.Metric.Unit}</p>
           </div>
-          <div className=''>
+          <div className='font-normal text-indigo-50 font-sans '>
             <p className=''>Wind: {weatherData.Wind.Direction.Degrees}/ {weatherData.Wind.Direction.English}</p>
             <p>Humidty: {weatherData.RelativeHumidity}</p>
             <p>Visibility: {weatherData.Visibility.Metric.Value} {weatherData.Visibility.Metric.Unit}</p>
